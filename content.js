2e840b91410812825c052c0293faea081ba07e46
Christopher <kriss17388+dev@gmail.com>
2023-03-04 14:16:11 +0000

    added a href

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
 alert("Do some work!");
+
+// Open a new window with your pop-up content
+var newWindow = window.open("", "", "width=400,height=200");
+newWindow.document.write("<h1>Close the tab!</h1><br /><p>Instead, <a href='https://github.com/'>make a thing</a></p>");
+
+// Close the current tab
+window.close();
