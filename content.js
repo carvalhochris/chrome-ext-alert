decb8a8764e3bed2e64fee390062688bded4f266
Christopher <kriss17388+dev@gmail.com>
2023-03-04 15:16:05 +0000

    redirect

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 alert("Do some work!");
 
+// Redirect the current page to the desired URL
+window.location.href = "https://github.com/carvalhochris";
+
 // Open a new window with your pop-up content
-var newWindow = window.open("", "", "width=400,height=200");
-newWindow.document.write("<h1>Close the tab!</h1><br /><p>Instead, <a href='https://github.com/'>make a thing</a></p>");
+// var newWindow = window.open("", "", "width=400,height=200");
+// newWindow.document.write("<h1>Close the tab!</h1><br /><p>Instead, <a href='https://github.com/'>make a thing</a></p>");
 
 // Close the current tab
-window.close();
+// window.close();
